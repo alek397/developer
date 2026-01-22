18432ff51446639d2ae97fa752cff22f4b136559
alek.git <alek17178@gmail.com>
2026-01-22 21:25:52 +0700

    2. if else

---
+++ java-dasar/src/main/java/org/example/IfElse.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int umur = 20;
+
+        if (umur >=18){
+            System.out.println("saya sudah dewasa");
+        } else {
+            System.out.println("saya masi kecil");
+        }
+    }
+}
