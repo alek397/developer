2c0ab2a79dbccb11c6c1ad9ec44ddaaf73c515b5
alek.git <alek17178@gmail.com>
2026-01-22 22:18:33 +0700

    soal ke 3

---
+++ java-dasar/src/main/java/org/example/Test.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+
+public class Test {
+
+
+//    #soal#
+//    ✅ Latihan 1 (Paling Dasar)
+//
+//    Buat program Java yang:
+//
+//    Menyimpan nama kamu ke variabel String nama
+//
+//    Menyimpan umur kamu ke variabel int umur
+//
+//    Menampilkan output:
+//
+//    Nama saya: <nama>
+//    Umur saya: <umur>
+//
+//    Contoh output kalau namanya Andi, umur 20:
+//    Nama saya: Andi
+//    Umur saya: 20
+
+//    #jawaban#
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        // masukan nama kamu
+//        System.out.print("Nama saya: ");
+//        String nama = input.nextLine();
+//        // masukan umur kamu
+//        System.out.print("Umur saya: ");
+//        int umur = input.nextInt();
+//        // hasil dari keduanya
+//        System.out.println("Nama saya: " + nama + ", Umur kamu: " + umur);
+//    }
+
+//    #soal#
+//            🎯 Latihan 2 (Penjumlahan)
+//
+//    Buat program yang:
+//
+//    Input 2 angka
+//
+//    Output hasil penjumlahannya
+//
+//    Contoh output:
+//    Masukkan angka 1: 10
+//    Masukkan angka 2: 5
+//    Hasil: 15
+//
+//            💡 Petunjuk (kamu boleh pakai ini)
+//    int angka1 = input.nextInt();
+//    int angka2 = input.nextInt();
+//    int hasil = angka1 + angka2;
+//System.out.println("Hasil: " + hasil);
+
+//#jawaban#
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Masukan angka: ");
+//        int angka1 = input.nextInt();
+//        System.out.print("Masukan angka: ");
+//        int angka2 = input.nextInt();
+//
+//        int hasil = angka1 + angka2;
+//        System.out.println("Hasil: " + hasil);
+//    }
+
+}
