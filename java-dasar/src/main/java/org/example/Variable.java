8f5d73c6abcecbe163252842ef2abcd866e65c64
alek.git <alek17178@gmail.com>
2026-01-22 21:22:20 +0700

    1. variable

---
+++ java-dasar/src/main/java/org/example/Variable.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Variable {
+    public static void main(String[] args) {
+        int umur = 20;
+        String nama = "alek";
+
+        System.out.println("nama saya adalah: " + nama);
+        System.out.println("umur saya adalah: " + umur);
+    }
+}
